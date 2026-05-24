1bca367b2abebed7f3c8f2d37411fae18cf8d675
Sasho Ristevski <sashoafs@gmail.com>
2026-05-24 14:54:04 +0200

    fix email notification

--- internal/mail/smtp.go
+++ internal/mail/smtp.go
@@ -23,5 +23,12 @@ func BookingConfirmation(host string, port int, user, password, fromAddr, toUser
 		"From: %s\r\nTo: %s\r\nSubject: %s\r\nMIME-Version: 1.0\r\nContent-Type: text/plain; charset=UTF-8\r\n\r\n%s\r\n",
 		fromAddr, toUser, subject, body,
 	))
-	return smtp.SendMail(addr, auth, fromAddr, []string{toUser}, msg)
+	return smtp.SendMail(addr, auth, extractEmail(fromAddr), []string{toUser}, msg)
+}
+
+func extractEmail(addr string) string {
+    if i := strings.Index(addr, "<"); i >= 0 {
+        return strings.TrimSuffix(strings.TrimSpace(addr[i+1:]), ">")
+    }
+    return addr
 }
