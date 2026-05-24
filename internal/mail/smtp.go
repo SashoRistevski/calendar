32e8e7b0d4e70d4efd9ddb1f85a122d076851db9
Sasho Ristevski <sashoafs@gmail.com>
2026-05-24 16:20:44 +0200

    test emal

--- internal/mail/smtp.go
+++ internal/mail/smtp.go
@@ -18,12 +18,52 @@ func BookingConfirmation(host string, port int, user, password, fromAddr, toUser
 	}
 
 	addr := fmt.Sprintf("%s:%d", host, port)
+
+	conn, err := net.DialTimeout("tcp", addr, 10*time.Second)
+	if err != nil {
+		return fmt.Errorf("dial: %w", err)
+	}
+	conn.SetDeadline(time.Now().Add(30 * time.Second))
+
+	c, err := smtp.NewClient(conn, host)
+	if err != nil {
+		return fmt.Errorf("smtp client: %w", err)
+	}
+	defer c.Close()
+
+	if ok, _ := c.Extension("STARTTLS"); ok {
+		if err := c.StartTLS(nil); err != nil {
+			return fmt.Errorf("starttls: %w", err)
+		}
+	}
+
 	auth := smtp.PlainAuth("", user, password, host)
-	msg := []byte(fmt.Sprintf(
+	if err := c.Auth(auth); err != nil {
+		return fmt.Errorf("auth: %w", err)
+	}
+
+	if err := c.Mail(extractEmail(fromAddr)); err != nil {
+		return fmt.Errorf("mail from: %w", err)
+	}
+	if err := c.Rcpt(toUser); err != nil {
+		return fmt.Errorf("rcpt: %w", err)
+	}
+
+	wc, err := c.Data()
+	if err != nil {
+		return fmt.Errorf("data: %w", err)
+	}
+	msg := fmt.Sprintf(
 		"From: %s\r\nTo: %s\r\nSubject: %s\r\nMIME-Version: 1.0\r\nContent-Type: text/plain; charset=UTF-8\r\n\r\n%s\r\n",
 		fromAddr, toUser, subject, body,
-	))
-	return smtp.SendMail(addr, auth, extractEmail(fromAddr), []string{toUser}, msg)
+	)
+	if _, err := fmt.Fprint(wc, msg); err != nil {
+		return fmt.Errorf("write: %w", err)
+	}
+	if err := wc.Close(); err != nil {
+		return fmt.Errorf("close data: %w", err)
+	}
+	return c.Quit()
 }
 
 func extractEmail(addr string) string {
